7bb8a48001fd09d86fb632512a0f6740d0868879
gu-geonhoe <rjsghl92@gmail.com>
2022-09-24 23:17:18 +0900

    3

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-BUILD_JAR=$(ls /home/ubuntu/action/build/libs/seb39_main_046-0.0.1-SNAPSHOT.jar)
+BUILD_JAR=$(ls /home/ubuntu/action/build/libs/ec2test-0.0.1-SNAPSHOT.jar)
 JAR_NAME=$(basename $BUILD_JAR)
 
 echo "> 현재 시간: $(date)" >> /home/ubuntu/action/deploy.log

a7199dd8fbc64332d12144f28cf48b6ea8410b7b
gu-geonhoe <rjsghl92@gmail.com>
2022-09-25 02:04:40 +0900

    17

--- src/main/java/com/example/Api/member/MemberService.java
+++ src/main/java/com/example/Api/member/MemberService.java
@@ -43,9 +43,9 @@ public class MemberService {
         Member member6 = new Member(6L,"string","관리자5",password2);
         member6.setRoles("ROLE_ADMIN");
         memberRepository.save(member6);
-        Member member6 = new Member(7L,"aaa","관리자7",password2);
+        Member member7 = new Member(7L,"aaa","관리자7",password2);
         member6.setRoles("ROLE_ADMIN");
-        memberRepository.save(member6);
+        memberRepository.save(member7);
         }
 
     public MemberService(MemberRepository memberRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {

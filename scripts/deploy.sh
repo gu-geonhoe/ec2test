c580283882bc99eede327f682a5956e35a8db424
gu-geonhoe <rjsghl92@gmail.com>
2022-09-26 03:58:42 +0900

    git action test42

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,10 +10,7 @@ echo "> build 파일 복사" >> /home/ubuntu/action/deploy.log
 DEPLOY_PATH=/home/ubuntu/action/
 cp $BUILD_JAR $DEPLOY_PATH
 
-#echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/action/deploy.log
-#CURRENT_PID=$(pgrep -f $JAR_NAME)
-
-echo "> 8080 PORT 에서 pid 확인"
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/action/deploy.log
 CURRENT_PID=$(lsof -ti tcp:8080)
 
 if [ -z $CURRENT_PID ]
@@ -24,8 +21,13 @@ else
   sudo kill -15 $CURRENT_PID
   sleep 5
 fi
-
+echo "4914" >> /home/ubuntu/action/deploy.log
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/action/deploy.log
-nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/action/deploy_err.log &
+sudo nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/action/deploy_err.log &
+echo "> build 명령어: $DEPLOY_JAR" >> /home/ubuntu/action/deploy.log
+
+
+
+

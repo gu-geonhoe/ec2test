7363f50c2bd5ae49f3fc70eb877b17eaab55c560
gu-geonhoe <rjsghl92@gmail.com>
2022-09-25 19:32:13 +0900

    git action test15

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -27,6 +27,6 @@ fi
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/action/deploy.log
 source ~/.bashrc
-sudo nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/action/deploy_err.log &
+nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/action/deploy_err.log &
 echo "> build 명령어: $DEPLOY_JAR" >> /home/ubuntu/action/deploy.log
 

b7909db68233cd3f495e5daf29681b05dae75856
gu-geonhoe <rjsghl92@gmail.com>
2022-09-26 00:45:58 +0900

    git action test24

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -26,7 +26,7 @@ fi
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/action/deploy.log
 source ~/.bashrc
-nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/action/deploy_err.log &
+sudo nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/action/deploy_err.log &
 echo "> build 명령어: $DEPLOY_JAR" >> /home/ubuntu/action/deploy.log
 echo "제발 돼라222" >> /home/ubuntu/action/deploy.log
 
